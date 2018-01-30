bbc72402fae60ab4749da967d14b7aa160507990
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2018-01-30 19:00:48 -0200

    criando redirect para a pagina inicial e utilizando HTTP (e não HTTPS)

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const PORT = process.env.PORT || 3000;
 
 express()
     .use(express.static(path.join(__dirname, 'dist')))
+    .get('*', (req, res) => res.redirect('http://' + req.headers.host) )
     .listen( PORT, () => console.log("Listening on port =>", PORT))
